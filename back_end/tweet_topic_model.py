1509c36dca5c27946c252dc69dc87e718618f8d2
Rahul Soni <sn.rahul99@gmail.com>
2018-04-15 20:47:47 +0800

    Added book ID

--- back_end/tweet_topic_model.py
+++ back_end/tweet_topic_model.py
@@ -1,19 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys, os
 import numpy as np 
 sys.path.append('./')
-from sklearn import metrics
 from sklearn.externals import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy import sparse
 import pickle, json
-from time import time
+import pandas as pd
 import nltk, re
 from nltk.corpus import stopwords
-from datetime import timedelta
+import subprocess
+from models import book_topic_model
 
 
+###################################################
+# 			Pre-processing component 			  #
+###################################################
 def rm_html_tags(str):
     html_prog = re.compile(r'<[^>]+>',re.S)
     return html_prog.sub('', str)
@@ -61,47 +63,21 @@ def pre_process(str, porter):
         try:
             str = [porter.stem(t) for t in str]
         except:
-            print(str)
+            #print(str)
             pass
     except:
-        print(str)
+        #print(str)
         pass
         
     return str
 
-def get_20news_vocab_and_data(porter, stops, vocab_path, data_path, data_labels):
-	if(args.use_filter):
-		remove = ('headers', 'footers', 'quotes')
-	else:
-		remove = ()
-
-	newsgroups = fetch_20newsgroups(subset=args.mode, remove=remove)
-
-	vocab = []; data = []
-	with open(data_path, 'w') as f:
-		for i in tqdm(range(len(newsgroups.data))):
-			news_i = newsgroups.data[i]
-			news_i = pre_process(news_i, porter)
-			news_i = [word for word in news_i if word not in stops]
-			f.write('%s\n' %news_i) # write new data to file
-			vocab.append(list(set(news_i))) # maintain unique words in a vocab
-
-	# write labels into file
-	np.savetxt(data_labels, newsgroups.target, delimiter=',', fmt='%i')
-
-	# write vocabulary into file
-	if(args.mode == 'train'):
-		flatten = lambda l: [item for sublist in l for item in sublist]
-		vocab = flatten(vocab)
-		vocab = list(set(vocab))
-		with open(vocab_path, 'w') as f:
-			pickle.dump(vocab, f)
-
-
-def get_topic_ids(tweets, tfidf_path, pca_path, mean_X_path, model_path):
+###################################################
+# 		Tweet Classification Component			  #
+###################################################
+def get_topic_ids(tweets_list, tfidf_path, pca_path, mean_X_path, model_path, mode):
 	model = joblib.load(model_path)
-	pca = pickle.load(open(pca_path, 'rb'))
-	tfidf_vocab = pickle.load(open(tfidf_path, 'rb'))
+	pca = pickle.load(open(pca_path, 'rb'), encoding='latin1')
+	tfidf_vocab = pickle.load(open(tfidf_path, 'rb'), encoding='latin1')
 	MEAN_X = np.loadtxt(mean_X_path, dtype=float, delimiter=',')
 	vectorizer = TfidfVectorizer(decode_error="replace", vocabulary=tfidf_vocab)
 
@@ -109,7 +85,9 @@ def get_topic_ids(tweets, tfidf_path, pca_path, mean_X_path, model_path):
 	stops = set(stopwords.words('english'))
 	stops.add('rt')
 
-	tweets_list = json.load(open(tweets))['tweets']
+	if(mode == 'validate'):
+		tweets_list = json.load(open(tweets_list))['tweets']
+
 	X = []
 	for tweet in tweets_list:
 		tweet = pre_process(tweet.strip('\n'), porter)
@@ -122,9 +100,16 @@ def get_topic_ids(tweets, tfidf_path, pca_path, mean_X_path, model_path):
 	ids = model.predict(X)
 	return(ids)
 
-if __name__ == "__main__":
 
-	#json_tweets = sys.argv[1]
+def main():
+
+	mode = 'test' # one of "test" or "validate"
+
+	if(mode == 'test'):
+		json_string = sys.argv[1]
+		json_string = json_string[1:-3]
+		json_tweets = json.loads(json_sring)
+		json_tweets = json_tweets['tweets']
 
 	# directory setup
 	proj_path = os.path.dirname(os.path.realpath(__file__))
@@ -136,14 +121,27 @@ if __name__ == "__main__":
 	pca_feat = os.path.join(model_basepath, 'pca_components.pkl')
 	model_path = os.path.join(model_basepath, 'rf_model.pkl')
 
-	json_tweets = os.path.join(data_basepath, 'test.json')
+	goodbooks_path = os.path.join(proj_path, 'data/goodbooks')
+	book2tag_path = os.path.join(goodbooks_path, 'book_tags.csv')
+	book_data_path = os.path.join(goodbooks_path, 'books.csv')
 
-	ids = get_topic_ids(tweets=json_tweets,
-				 tfidf_path=tfidf_feat,
-				 pca_path=pca_feat,
-				 mean_X_path=mean_X_feat,
-				 model_path=model_path,
-			  		 )
+	if(mode == 'validate'):
+		json_tweets = os.path.join(data_basepath, 'test.json')
 
-	print ids
-	
+	ids = get_topic_ids(tweets_list=json_tweets,
+						tfidf_path=tfidf_feat,
+						pca_path=pca_feat,
+						mean_X_path=mean_X_feat,
+						model_path=model_path,
+						mode=mode,
+			  			)
+
+	json_out = book_topic_model.top_recommended_books(tweet_labels=ids,
+													  book2tag_path=book2tag_path,
+													  book_data_path=book_data_path,
+													 )
+
+	print(json_out)
+
+if __name__ == "__main__":
+	main()
